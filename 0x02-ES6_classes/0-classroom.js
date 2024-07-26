e250bf6fa6067bb795f8a7e93bb807cfad86abdb
AhmedYousif <ahmedmax0909090909@gmail.com>
2024-07-26 01:22:42 +0100

    add indentation

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,11 +2,11 @@
  * Represents a class room class
  */
 export default class ClassRoom {
-/**
- * Creates a new class
- * @param {Number} maxStudentsSize - the maximum number of
- * students in a class. 
- */
+  /**
+   * Creates a new class
+   * @param {Number} maxStudentsSize - the maximum number of
+   * students in a class. 
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
