e14ff29fac4666a06a07cd85b36a6736187bd6c2
AhmedYousif <ahmedmax0909090909@gmail.com>
2024-07-26 01:20:31 +0100

    update

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,12 @@
+/**
+ * Represents a class room class
+ */
 export default class ClassRoom {
+/**
+ * Creates a new class
+ * @param {Number} maxStudentsSize - the maximum number of
+ * students in a class. 
+ */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
