309e27c9580ed7a1d3c39821242a474ba3be12f8
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-09-18 11:52:37 +0000

    task done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the stdout,
+ * @param {String} msg the message to display.
+ */
+const displayMessage = (msg) => {
+  console.log(msh);
+};
+
+module.exports = displayMessage;
